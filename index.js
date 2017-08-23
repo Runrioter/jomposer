1270e45d0b3757640f048f54bc9ba2f0a01ad490
Runrioter <runrioter@qq.com>
2017-08-23 10:22:55 +0800

    extract logger to lib

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const exec = require('child_process').exec;
 const co = require('co');
-const DataFetcher = require('./libs/data-fetcher');
-const hashFile = require('./libs/hash-file');
+const DataFetcher = require('./lib/data-fetcher');
+const hashFile = require('./lib/hash-file');
+const logger = require('./lib/logger');
 const fs = require('fs');
 const path = require('path');
 
@@ -21,28 +22,28 @@ co(function *() {
   const expectedSig = yield df.getComposerInstallerSignature();
   
   if (currentSig === expectedSig) {
-    df.info('Installer signatures matched successfully');
+    logger.info('Installer signatures matched successfully');
     try {
-      df.info('Downloading Composer...');
+      logger.info('Downloading Composer...');
       const [outMsg, errMsg] = yield installComposer(setUpFile);
       if (outMsg) {
         //For example: Composer (version 1.5.1)
         const found = outMsg.match(/Composer (\(version \d+(\.\d)*\S*\))/);
-        df.info(`${found[0]} successfully installed`);
-        df.info(`Deleting Composer Installer`);
+        logger.info(`${found[0]} successfully installed`);
+        logger.info(`Deleting Composer Installer`);
         fs.unlinkSync(setUpFile);
       } else if (errMsg) {
-        df.error(errMsg);
+        logger.error(errMsg);
       }
     } catch(err) {
-      df.error(err.message);
+      logger.error(err.message);
       process.exit(1);
     }
   } else {
-    df.error('ERROR: Invalid installer signature');
+    logger.error('ERROR: Invalid installer signature');
     process.exit(1);
   }
 }).catch(err => {
-  df.error(err.stack);
+  logger.error(err.stack);
   process.exit(1);
 });
